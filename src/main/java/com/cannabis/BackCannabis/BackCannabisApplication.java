a660d855f6cc06d29d64d02e9d2b7b010ae99e78
WI1L0 <william.martinez.est@tecazuay.edu.ec>
2023-05-11 16:41:05 -0500

    primero

---
+++ src/main/java/com/cannabis/BackCannabis/BackCannabisApplication.java
@@ -0,0 +1,13 @@
+package com.cannabis.BackCannabis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCannabisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCannabisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cannabis/BackCannabis/BackCannabisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cannabis.BackCannabis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCannabisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
